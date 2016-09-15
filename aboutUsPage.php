545432d892c1e3ccf6fcf81b0c8106c1b2f16a6a
Ploy Kitisopakul <kki32@uclive.ac.nz>
2016-09-15 19:33:06 +1200

    tidy up doc

--- aboutUsPage.php
+++ aboutUsPage.php
@@ -22,11 +22,11 @@
     <?php include("includes/header.html");?>
 
     <div class="center" id="search-container">
-      <h3 class="header">About Seafood</h3>
+      <h3 class="header">About Seafood Version 1.0</h3>
       <div class="aboutUs-container">
         <p class="paragraph">
-          Seafood is a perfect website for people who wants to quickly search the restaurant in Christchurch by the food they want, the atmosphere they want, the theme they want, or the location they want.
-          It is also good for people who runs out of idea of what they want to eat.</p>
+          Seafood is a perfect website for user who wants to quickly search the restaurant in Christchurch by the food they want, the atmosphere they want, the theme they want, or the location they want. In the future, we aim to make it like a diary for users to keep a record of the restaurants the users enjoy.
+
         </div>
 
         <h3 class="header">Implemented Feature</h3>
@@ -35,13 +35,11 @@
           <p class="paragraph">
             At the moment, we have</p>
             <ul>
-              <li class="bullet-points" >implemented the interface of the website using HTML5 and Css.</li>
-              <li class="bullet-points" >implemented at least two JQuery plugins. First is the slider for price range under Search tab and second is the progress bar when translating our language to Yoda language.
-                Second one is in </li>
-                <li class="bullet-points" >implemented the facebook comments under review of the restaurant</li>
-                <li class="bullet-points" >implemented the facebook comments under review of the restaurant</li>
-                <li class="bullet-points" >made API call to translate our language to Yoda language :) in About Us tab under Say hi to us in yoda language header</li>
-  <li class="bullet-points" > implemented Bootstrap such as the command prediction under Search tab. Try typing T in the textfield under Cuisine header and you would see Thai.</li>
+              <li class="bullet-points" >implemented the interface of the website using HTML5 and CSS.</li>
+              <li class="bullet-points" >implemented at least two JQuery plugins. First is the slider for price range under Search page and second is the progress bar when translating our language to Yoda language under About Us page.</li>
+                <li class="bullet-points" >implemented the Facebook Comments under review of the restaurant.</li>
+                <li class="bullet-points" >made API call to translate our language to Yoda language :) in About Us page under Say hi to us in yoda language header.</li>
+  <li class="bullet-points" > implemented Bootstrap such as the command prediction under Search page. Try typing T in the textfield under Cuisine header and you would see Thai.</li>
               </ul>
             </div>
 
@@ -57,7 +55,7 @@
                   <li class="bullet-points" >to allow user to rate the restaurant.</li>
                   <li class="bullet-points" >to sort the restaurant based on the selected button</li>
                   <li class="bullet-points" >to retrieve the opening hours from api</li>
-                  <li class="bullet-points" >to allow user to add questions to help tab</li>
+                  <li class="bullet-points" >to allow user to add questions to help page</li>
                   <li class="bullet-points" >to search for the restaurant using the category</li>
                   <li class="bullet-points" >to create a list of recommendation under Browse so if the user has no idea what they want, they could look at other people's list</li>
                   <li class="bullet-points" >to expand restaurant database to include restaurants outside Christchurch.</li>
@@ -68,8 +66,10 @@
               <div class="aboutUs-container">
                 <p class="paragraph">
                   Our designer is Kanyakorn Kitisopakul (Ploy) who is currently studying 3rd year of Software Engineering at University of Canterbury.
+
                 </p>
               </div>
+
 
               <h3 class="header">Say hi to us in yoda language  :)</h3>
               <div class="aboutUs-container">
@@ -81,7 +81,7 @@
                 <input type="submit" value="Send" onclick="progressBar()"></input>
               </div>
 
-
+        <h3 class="header">  If in doubt with anything on the website, see Help page</h3>
 
 
             </div>
